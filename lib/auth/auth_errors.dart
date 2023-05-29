093fe739855894e53f82f9547e5efebcf4762371
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-29 14:15:09 +0500

    auth errors

--- lib/auth/auth_errors.dart
+++ lib/auth/auth_errors.dart
@@ -5,4 +5,8 @@ import 'package:flutter/foundation.dart' show immutable;
 abstract class AuthErrors {
   final String dialogeTitle;
   final String dialogeText;
+  AuthErrors({
+    required this.dialogeTitle,
+    required this.dialogeText,
+  });
 }
