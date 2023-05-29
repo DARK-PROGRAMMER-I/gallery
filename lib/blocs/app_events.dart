6dc52d47c6ca230c8c6194357f05deb6628a0918
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-29 20:38:26 +0500

    Bloc Events

--- lib/blocs/app_events.dart
+++ lib/blocs/app_events.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/foundation.dart' show immutable;
 
+// Events
+
 @immutable
 abstract class AppEvents{
   const AppEvents();
 }
 
+
 @immutable
 class AppEventsUploadImage implements AppEvents{
   final String imagePath;
-  AppEventsUploadImage({required this.imagePath});
+  const AppEventsUploadImage({required this.imagePath});
+}
+
+@immutable
+class AppEventsDeleteAccount implements AppEvents{
+  const AppEventsDeleteAccount();
+}
+
+@immutable
+class AppEventsLogOut implements AppEvents{
+  const AppEventsLogOut();
+}
+
+@immutable
+class AppEventsLogIn implements AppEvents{
+  final
+  const AppEventsLogOut();
 }
